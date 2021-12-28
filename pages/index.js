39cb82e77ca83bc1ec42cd6e42cfafc22bf85745
Augusto Seabra <augusto.seabra00@gmail.com>
2021-12-28 13:25:07 -0300

    primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react"
+
+function Home() {
+  return (
+    <div>
+      <h1> Home </h1>
+      <Contador />
+    </div>
+  )
+}
+
+function Contador() {
+  const [contador, setContador] = useState(1)
+
+  const adicionarContador = () => {
+    setContador(contador + 1)
+  }
+
+  return (
+    <div>
+      <p>{contador}</p>
+      <button onClick={adicionarContador}>Adicionar</button>
+    </div>
+  )
+}
+
+export default Home
